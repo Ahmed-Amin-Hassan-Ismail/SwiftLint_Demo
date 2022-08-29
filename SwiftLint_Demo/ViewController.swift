5881c65828593bb80cecfe7bcee0074db9b63236
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-08-29 23:48:06 +0200

    fixing swiftLint convension instruction and do some comparation

--- SwiftLint_Demo/ViewController.swift
+++ SwiftLint_Demo/ViewController.swift
@@ -14,6 +14,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
 }
-
+/*
+extension ViewController {
+    // MARK: - About to look at some grabage
+    
+    // this is some poorly formatted comments
+    
+    open func ReallyAwfullyMethodThatDoesDanGEROUSSTUFF(numberOne: Any?, numberTwo: Any?) -> Any? {
+        let formattedNumberTwo = numberTwo as! Int
+        var runningSum: Int?
+        
+        for _ in 0..<formattedNumberTwo {
+            runningSum! += numberOne as! Int
+        }
+        return runningSum!
+    }
+}
+*/
+ extension ViewController {
+     // MARK: - About to look at some grabage
+     // this is some poorly formatted comments
+     open func reallyAwfullyMethodThatDoesDanGEROUSSTUFF(numberOne: Int, numberTwo: Int) -> Int {
+         let formattedNumberTwo = numberTwo
+         var runningSum: Int!
+         for _ in 0..<formattedNumberTwo {
+             runningSum += numberOne
+         }
+         return runningSum
+     }
+ }
