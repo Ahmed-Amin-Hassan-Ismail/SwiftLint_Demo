11632efad0437685c719f846cdf7245cde2a0bda
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-08-29 23:21:52 +0200

    first commit

---
+++ SwiftLint_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftLint_Demo
+//
+//  Created by Ahmed Amin on 29/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
